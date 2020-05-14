4cfb9e1205db42240812dd70edaacf29caaae683
Dunia Nasr <dunia.nasr777@gmail.com>
2020-05-14 20:00:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solutions in this file
+writeCards=(["Ada", "Brendan", "Ali"], "birthday");
+function card(writeCards){
+  for(let i=0;i<writeCards.length;i++){
+    console.log('Thank you,${writeCards[i]} for the wonderful birthday gift!');
+  }
+  return card;
+}
+function count(countDown){
+  let i=0;
+  while()
+}
